8e17f003aca293552f7d7b93086ad052453ed051
Mahmoud Yousefi <mufimf14@gmail.com>
2025-01-14 23:58:42 -0800

    `Update Docker Compose and NestJS configuration for PostgreSQL database`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ItemsModule } from './items/items.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}), 
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.PG_HOST,
+      port: parseInt(process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    ItemsModule, 
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/database/database.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.getOrThrow('PG_HOST'),
+                port: configService.getOrThrow('PG_PORT'),
+                database: configService.getOrThrow('PG_DB'),
+                username: configService.getOrThrow('PG_USER'),
+                password: configService.getOrThrow('PG_PASSWORD'),
+                autoLoadEntities: true,
+                synchronize: true,
+            }),
+            inject: [ConfigService],
+        })
+    ]
+})
+export class DatabaseModule {}
